db751ff9449a86393658494c56b6f01e3a616277
isaacachenbach <isaac.achenbach1@gmail.com>
2023-05-31 16:25:38 -0500

    added weather map project to new repo

---
+++ js/keys.js
@@ -0,0 +1,6 @@
+
+export const keys = {
+    openweather: '044b1cf6d4e95372f96defa7bdc19d3d',
+    mapbox: 'pk.eyJ1IjoiaXNhYWNhY2giLCJhIjoiY2xmbGFqNnpvMDAxODNvbzk1czVxZng0bSJ9.c6lz-jV09G3S_pfICpIqFA',
+    github: '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'
+}
